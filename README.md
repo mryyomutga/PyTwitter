7cf00e3aed597e88fa7dc298bbd6b3d93bbc50b3
mryyomutga <mryyomutga@gmail.com>
2017-11-01 20:48:24 +0900

    1st commit

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # Pytwitter
 twitterの分析プログラム
+
+PythonでTwitter APIへアクセスしていろいろやる
+
+## やること
+ライブラリに`Requests_OAuthlib`を用いるため、それのインストール
+
+`$ pip install requests requests_oauthlib`
+
+Twitter APIにアクセスするためにアプリケーションの登録をする
+
+[Twitterの開発者向けサイト](https://apps.twitter.com/app/new)にアクセスして
+- Consumer key
+- Consumer secret
+- Access token
+- Access token secret
+
+を取得する
+
+細かいやり方は[ここ](http://website-planner.com/twitter%E3%82%A2%E3%83%97%E3%83%AA%E3%82%B1%E3%83%BC%E3%82%B7%E3%83%A7%E3%83%B3%E3%81%AE%E4%BD%9C%E6%88%90%EF%BC%88consumer-key%E3%80%81consumer-secret%E3%80%81access-token%E3%80%81access-token-secret/)
+
+## ライブラリ
+- Requests-OAuthlib(OAuth認証、リクエスト...)
+- json
+
+## 説明
+- Pytwitter.pyのtweet関数でツイートすることができる
+- Pytwitter.pyのresearch関数でキーワードから50件のツイートを取得します
---
+++ src/Pytwitter.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+# Pytwitter.py
+# Author    : Ryoga Miyamoto
+from requests_oauthlib import OAuth1Session
+import json
+
+# リクエスト送信用URL
+URL = {
+        "tweet":"https://api.twitter.com/1.1/statuses/update.json",
+        "search":"https://api.twitter.com/1.1/search/tweets.json"
+}
+
+# MyTwitterAPIKey.jsonからTwitterAPIの読み出し
+with open("TwitterAPIKey.json", "r") as f:
+    apiKey = json.load(f)
+
+# Tweetを行う
+def tweet():
+    # ツイートフォーム
+    print("つぶやく内容を入力してください")
+    message = input(">> ")
+    print("------------------------------------------------")
+
+    params = {"status":message}
+    # OAuth認証をし、POSTリクエストでツイート
+    session = OAuth1Session(apiKey["CK"], apiKey["CS"], apiKey["AT"], apiKey["AS"])
+    req = session.post(URL["tweet"], params=params)
+
+    if req.status_code == 200:
+        print("<< Succeed!")
+        print(message)
+        log.append(message)
+    else:
+        print("<< ERROR! : {0}".format(req.status_code))
+
+def research():
+    global log
+    print("検索する内容を入力してください")
+    keyword = input(">> ")
+    params = {"q":keyword,
+              "locale":"ja",
+              "result_type":"recent",
+              "count":50}
+
+    # OAuth認証をし、GETリクエストで検索
+    session = OAuth1Session(apiKey["CK"], apiKey["CS"], apiKey["AT"], apiKey["AS"])
+    res = session.get(URL["search"], params = params)
+
+    # レスポンスをJSON形式に変換
+    tweets = json.loads(res.text)
+
+    if res.status_code == 200:
+        print("<< Succeed!\n")
+        for tweet in tweets["statuses"]:
+            mes = "@{0}: {1}\nDate: {2}\n{3}\n".format(
+                tweet["user"]["screen_name"],
+                tweet["user"]["name"],
+                tweet["created_at"],
+                tweet["text"]
+                )
+            print(mes)
+            with open("tweet.json", "w", encoding="cp932") as f:
+                f.write(json.dumps(tweet))
+
+            log.append(mes)
+    else:
+        print("<< ERROR! : {0}".format(req.status_code))
+
+# 取得したツイートのログリスト
+log = list()
+
+tweet()
+# research()
+
+# with open("PytwitterLog.log", "a+", encoding="utf-8") as f:
+#     for l in log:
+#         f.write(l)
