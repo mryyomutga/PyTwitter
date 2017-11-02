b47cd965e29e39225ffd719ea116111c6f2f09e1
mryyomutga <mryyomutga@gmail.com>
2017-11-02 12:56:26 +0900

    繰り返しできるように改造

--- README.md
+++ README.md
@@ -3,22 +3,55 @@ twitterの分析プログラム
 
 PythonでTwitter APIへアクセスしていろいろやる
 
-## やること
+## TODO
+- トレンドの取得について機能改善(trendとvolumeだけは寂しい)
+- プロフィール変更できるようにする?
+- ~~繰り返し実行可能にするために、コマンド対応表を作成する~~(実装したものだけ)
+- ~~ツイートに"Pytwitterからの投稿"を組込む~~
+- パッケージ化なり外部からimportなりできるようにし、実行専用スクリプトにするとよし
+- Twitter機能を実装したメソッドをもう少し加える
+- プログラム設計について考える
+- リファクタリング
+- GUIなど、PytwitterのUIを実装できるとGood!!
+
+## 必要なこと
 ライブラリに`Requests_OAuthlib`を用いるため、それのインストール
 
 `$ pip install requests requests_oauthlib`
 
-Twitter APIにアクセスするためにアプリケーションの登録をする
+1. Twitter APIにアクセスするためにアプリケーションの登録をする
 
-[Twitterの開発者向けサイト](https://apps.twitter.com/app/new)にアクセスして
-- Consumer key
-- Consumer secret
-- Access token
-- Access token secret
+	[Twitterの開発者向けサイト](https://apps.twitter.com/app/new)にアクセスして
+	- Consumer key
+	- Consumer secret
+	- Access token
+	- Access token secret
 
-を取得する
+	を取得する
 
-細かいやり方は[ここ](http://website-planner.com/twitter%E3%82%A2%E3%83%97%E3%83%AA%E3%82%B1%E3%83%BC%E3%82%B7%E3%83%A7%E3%83%B3%E3%81%AE%E4%BD%9C%E6%88%90%EF%BC%88consumer-key%E3%80%81consumer-secret%E3%80%81access-token%E3%80%81access-token-secret/)
+	細かいやり方は[ここ](http://website-planner.com/twitter%E3%82%A2%E3%83%97%E3%83%AA%E3%82%B1%E3%83%BC%E3%82%B7%E3%83%A7%E3%83%B3%E3%81%AE%E4%BD%9C%E6%88%90%EF%BC%88consumer-key%E3%80%81consumer-secret%E3%80%81access-token%E3%80%81access-token-secret/)
+
+2. 取得した鍵をjsonファイルに書き込む
+	4つの鍵を取得したら`TwitterAPIKey.json`に設定を書き込む
+
+	TwitterAPIKey.jsonは
+	```json
+	{
+		"screen_name":"xxxx"
+		"CK":"xxxxxxxxxxxxxxx",
+		"CS":"xxxxxxxxxxxxxxx",
+		"AT":"xxxxxxxxxxxxxxx",
+		"AS":"xxxxxxxxxxxxxxx"
+	}
+
+	```
+
+	となっている。
+
+	`screen_name`にtwitterの`@名前`の@抜きのものを書く
+
+	`CK`,`CS`,`AT`,`AS`にはそれぞれのキーを書き込む
+
 
 ## ライブラリ
 - Requests-OAuthlib(OAuth認証、リクエスト...)
@@ -30,3 +63,5 @@ Twitter APIにアクセスするためにアプリケーションの登録をす
 - home_timeline関数で自分のタイムラインを取得
 - user_timeline関数で指定したユーザーのタイムラインを取得
 - place_trend関数で指定した地域のトレンドを取得
+- 指定したユーザーのフォロワーを取得
+- これらを繰り返し実行
--- src/Pytwitter.py
+++ src/Pytwitter.py
@@ -2,54 +2,27 @@
 # Pytwitter.py
 # Author    : Ryoga Miyamoto
 
+# Twitter APIのラッパーライブラリ(tweepy, python-twitter...)があるが直接APIを呼び出す
+
 # Python2でPython3用print()を使用するモジュール
 from __future__ import print_function
-# Requests,OAuth認証用ライブラリ
+# Requests,OAuth認証用ライブラリ(たぶんrequestsモジュールが内部にある)
 from requests_oauthlib import OAuth1Session
 # json形式データを扱うライブラリ
 import json
-
-# TODO: トレンドの取得について機能改善(trendとvolumeだけは寂しい)
-# TODO: プロフィール変更できるようにする
-# TODO: 繰り返し実行可能にするために、コマンド対応表を作成する
-# TODO: パッケージ化なり外部からimportなりできるようにし、実行専用スクリプトにするとよし
-# TODO: Twitter機能を実装したメソッドを加える
-# TODO: プログラム設計について考える
-# TODO: リファクタリング
-# TODO: GUIなど、PytwitterのUIを実装できるとGood!!
-# リクエスト送信用URL
-# URL:https://developer.twitter.com/en/docs
-URL = {
-    "tweet":"https://api.twitter.com/1.1/statuses/update.json",
-    "home_timeline":"https://api.twitter.com/1.1/statuses/home_timeline.json",
-    "user_timeline":"https://api.twitter.com/1.1/statuses/user_timeline.json",
-    "search":"https://api.twitter.com/1.1/search/tweets.json",
-    "place_trend":"https://api.twitter.com/1.1/trends/place.json"
-}
-
-# 取得したツイートのログリスト
-log = list()
-
-# TwitterAPIKey.jsonからKeyの読み出し
-with open("MyTwitterAPIKey.json", "r", encoding="utf-8") as f:
-    myinfo = json.load(f)
-
-# 地域情取得用WOEIDの読み出し
-# URL:https://shun1adhocblog.wordpress.com/2013/01/01/twitterapi%E3%82%92%E4%BD%BF%E3%81%A3%E3%81%A6%E3%81%BF%E3%82%8B%E3%83%86%E3%82%B9%E3%83%88by-python/
-with open("woeid.json", "r", encoding="utf-8") as f:
-    place_code = json.load(f)
-
-name = myinfo["screen_name"]
-termname = name + " >> "
+# 画面消去するos.system("cls")
+import os
 
 # Tweetを行う
+# ハッシュタグや@userなどを自動で検出する
 def tweet():
     # メッセージリスト
     mes_list = list()
+    # mes_list.append("Pytwitterからの投稿")
     # ツイートフォーム
-    print("つぶやく内容を入力してください(\"<<END\"で入力終了)")
+    print("つぶやく内容を入力してください(\"<END>\"で入力終了)")
     mes_list.append(input(termname))
-    while mes_list[-1] != "<<END":
+    while mes_list[-1] != "<END>":
         mes_list.append(input(termname))
 
     print("------------------------------------------------")
@@ -60,15 +33,15 @@ def tweet():
     # リクエストに付与するパラメータ
     params = {"status":mes}
 
-    # OAuth認証をし、POSTリクエストでツイート
+    # OAuth認証をし、POSTメソッドでツイート
     session = OAuth1Session(myinfo["CK"], myinfo["CS"], myinfo["AT"], myinfo["AS"])
     req = session.post(URL["tweet"], params=params)
 
     if req.status_code == 200:
-        print(name + " << Succeed!\n")
+        print("<< Succeed!\n")
         # log.append(mes)
     else:
-        print(name + " << ERROR! : {0}".format(req.status_code))
+        print("<< ERROR! : {0}".format(req.status_code))
 
 # 自分のタイムラインを取得する
 def home_timeline():
@@ -78,6 +51,8 @@ def home_timeline():
 
     # リクエストに付与するパラメータ
     params = {"count":50}
+
+    # OAuth認証をし、GETメソッドで自分のタイムラインを取得
     session = OAuth1Session(myinfo["CK"], myinfo["CS"], myinfo["AT"], myinfo["AS"])
     res = session.get(URL["home_timeline"], params=params)
 
@@ -85,10 +60,10 @@ def home_timeline():
     tweets = json.loads(res.text)
 
     if res.status_code == 200:
-        print(name + " << Succeed!")
-        print("------------------------------------------------")
+        print("<< Succeed!")
         for tweet in tweets:
-            mes = "@{0}: {1}\nDate: {2}\n{3}\n".format(
+            print("+----------------------------------------------+")
+            mes = " @{0}: {1}\n Date: {2}\n {3}".format(
                 tweet["user"]["screen_name"],
                 tweet["user"]["name"],
                 tweet["created_at"],
@@ -96,21 +71,22 @@ def home_timeline():
                 )
             print(mes)
             # log.append(message)
-        print("------------------------------------------------")
+        print("+----------------------------------------------+")
     else:
-        print(name + " << ERROR! : {0}".format(res.status_code))
+        print("<< ERROR! : {0}".format(res.status_code))
 
 # 指定したscreen_nameのタイムラインを取得
 def user_timeline():
-        print("指定したスクリーンネームのタイムラインを取得します")
+        print("指定したユーザーのタイムラインを取得します")
         print("@screen_nameを入力してください")
-        screen_name = input(termname)
+        user = input(termname)
         print("------------------------------------------------")
 
         # リクエストに付与するパラメータ
-        params = {"screen_name":screen_name,
+        params = {"screen_name":user,
                   "count":50}
 
+        # OAuth認証をし、GETメソッドでユーザーのタイムラインを取得
         session = OAuth1Session(myinfo["CK"], myinfo["CS"], myinfo["AT"], myinfo["AS"])
         res = session.get(URL["user_timeline"], params=params)
 
@@ -118,10 +94,10 @@ def user_timeline():
         tweets = json.loads(res.text)
 
         if res.status_code == 200:
-            print(name + " << Succeed!")
-            print("------------------------------------------------")
+            print("<< Succeed!")
             for tweet in tweets:
-                mes = "@{0}: {1}\nDate: {2}\n{3}\n".format(
+                print("+----------------------------------------------+")
+                mes = " @{0}: {1}\n Date: {2}\n {3}".format(
                     tweet["user"]["screen_name"],
                     tweet["user"]["name"],
                     tweet["created_at"],
@@ -129,12 +105,51 @@ def user_timeline():
                     )
                 print(mes)
                 # log.append(message)
-            print("------------------------------------------------")
+            print("+----------------------------------------------+")
         else:
-            print(name + " << ERROR! : {0}".format(res.status_code))
+            print("<< ERROR! : {0}".format(res.status_code))
+
+# 指定したユーザーのフォロワーを取得する
+def followers_list():
+    print("ユーザーを指定してください")
+    user = input(termname)
+    print("------------------------------------------------")
+
+    # リクエストに付与するパラメータ
+    params = {
+        "screen_name":user,
+        "count":200
+    }
+
+    # OAuth認証をし、GETメソッドでフォロワーを取得
+    session = OAuth1Session(myinfo["CK"], myinfo["CS"], myinfo["AT"], myinfo["AS"])
+    res = session.get(URL["followers_list"], params = params)
+
+    # レスポンスをJSON形式に変換
+    tweets = json.loads(res.text)
+
+    if res.status_code == 200:
+        print("<< Succeed!")
+        for tweet in tweets["users"]:
+            print("+----------------------------------------------+")
+            mes = " @{0}: {1}\n Created at {2}\n Followers: {3}\n Friends: {4}\n Location: {5}\n URL: {6}\n {7} tweet".format(
+                tweet["screen_name"],
+                tweet["name"],
+                tweet["created_at"],
+                tweet["followers_count"],
+                tweet["friends_count"],
+                tweet["location"],
+                tweet["url"],
+                tweet["statuses_count"]
+                )
+            print(mes)
+            # log.append(mes)
+        print("+----------------------------------------------+")
+    else:
+        print("<< ERROR! : {0}".format(req.status_code))
 
 # ツイート内容に対してキーワード検索を行う
-def research():
+def search():
     global log
     print("検索する内容を入力してください")
     keyword = input(termname)
@@ -145,7 +160,7 @@ def research():
               "result_type":"recent",
               "count":50}
 
-    # OAuth認証をし、GETリクエストで検索
+    # OAuth認証をし、GETメソッドでキーワード検索
     session = OAuth1Session(myinfo["CK"], myinfo["CS"], myinfo["AT"], myinfo["AS"])
     res = session.get(URL["search"], params = params)
 
@@ -153,10 +168,10 @@ def research():
     tweets = json.loads(res.text)
 
     if res.status_code == 200:
-        print(name + " << Succeed!")
-        print("------------------------------------------------")
+        print("<< Succeed!")
         for tweet in tweets["statuses"]:
-            mes = "@{0}: {1}\nDate: {2}\n{3}\n".format(
+            print("+----------------------------------------------+")
+            mes = " @{0}: {1}\n Date: {2}\n {3}".format(
                 tweet["user"]["screen_name"],
                 tweet["user"]["name"],
                 tweet["created_at"],
@@ -164,9 +179,9 @@ def research():
                 )
             print(mes)
             # log.append(mes)
-        print("------------------------------------------------")
+        print("+----------------------------------------------+")
     else:
-        print(name + " << ERROR! : {0}".format(req.status_code))
+        print("<< ERROR! : {0}".format(req.status_code))
 
 # 地域のトレンドを取得
 def place_trend():
@@ -177,6 +192,7 @@ def place_trend():
     # リクエストに付与するパラメータ
     params = {"id":place_code[place]["woeid"]}
 
+    # OAuth認証をし、GETメソッドで地域のトレンドを取得
     session = OAuth1Session(myinfo["CK"], myinfo["CS"], myinfo["AT"], myinfo["AS"])
     res = session.get(URL["place_trend"], params=params)
 
@@ -184,26 +200,122 @@ def place_trend():
     tweets = json.loads(res.text)
 
     if res.status_code == 200:
-        print(name + " << Succeed!")
-        print("------------------------------------------------")
+        print("<< Succeed!")
         # 他のJSONと少し違うせいか[0]["trends"]じゃないと取得できない
         for tweet in tweets[0]["trends"]:
-            mes = "trends: {}\nvolume: {}\n".format(
+            print("+----------------------------------------------+")
+            mes = " trends: {0}\n volume: {1}".format(
                 tweet["name"],
                 tweet["tweet_volume"]
                 )
             print(mes)
             # log.append(message)
-        print("------------------------------------------------")
+        print("+----------------------------------------------+")
     else:
-        print(name + " << ERROR! : {0}".format(res.status_code))
+        print("<< ERROR! : {0}".format(res.status_code))
 
-# Pytwitter実行
+# リクエスト送信用URL
+# Twitterの開発者用のページを参照
+# URL:https://developer.twitter.com/en/docs
+URL = {
+    "tweet":"https://api.twitter.com/1.1/statuses/update.json",
+    "home_timeline":"https://api.twitter.com/1.1/statuses/home_timeline.json",
+    "user_timeline":"https://api.twitter.com/1.1/statuses/user_timeline.json",
+    "followers_list":"https://api.twitter.com/1.1/followers/list.json",
+    "search":"https://api.twitter.com/1.1/search/tweets.json",
+    "place_trend":"https://api.twitter.com/1.1/trends/place.json"
+}
+
+# cmd_mesを表示する関数
+def print_cmd_mes():
+    print("+------------------+")
+    for s in cmd_mes:
+        print(s)
+    print("+------------------+")
+
+# 繰り返し実行する関数
+def twitter_main():
+    # プログラム終了フラグ
+    Escape = False
+
+    for (index, value) in enumerate(cmd_list):
+        cmd_mes.append(" {0:<2}: {1}".format(index + 1, value))
+
+    print("コマンドリスト")
+    print_cmd_mes()
+
+    # Pytwitterの起動
+    while Escape == False:
+        print("何をしますか?")
+        cmd = input(termname)
+        # 入力されたコマンドがリストに存在するか
+        if cmd in cmd_list:
+            # コンソール画面の消去
+            # OSの判定
+            if os.name == "nt":
+                os.system("CLS")
+            else:
+                os.system("clear")
+
+            # コマンドの実行
+            if cmd == cmd_list[0]:
+                tweet()
+            elif cmd == cmd_list[1]:
+                home_timeline()
+            elif cmd == cmd_list[2]:
+                user_timeline()
+            elif cmd == cmd_list[3]:
+                followers_list()
+            elif cmd == cmd_list[4]:
+                search()
+            elif cmd == cmd_list[5]:
+                place_trend()
+            elif cmd == cmd_list[6]:
+                print_cmd_mes()
+            elif cmd == cmd_list[7]:
+                Escape = True
+        else:
+            print("{0}はコマンドじゃないよ".format(cmd))
+
+    print("Good Bye!!")
+
+# 取得したツイートのログリスト
+log = list()
+
+# TwitterAPIKey.jsonからKeyの読み出し
+with open("TwitterAPIKey.json", "r", encoding="utf-8") as f:
+    myinfo = json.load(f)
+
+# 地域情取得用WOEIDの読み出し
+# URL:https://shun1adhocblog.wordpress.com/2013/01/01/twitterapi%E3%82%92%E4%BD%BF%E3%81%A3%E3%81%A6%E3%81%BF%E3%82%8B%E3%83%86%E3%82%B9%E3%83%88by-python/
+with open("woeid.json", "r", encoding="utf-8") as f:
+    place_code = json.load(f)
+
+# アカウント名を表示する場合
+name = "@" + myinfo["screen_name"]
+termname = name + " >> "
+# termname = ">> "
+
+# コマンドリスト
+cmd_mes = list()
+cmd_list =("tweet",
+           "home_timeline",
+           "user_timeline",
+           "followers_list",
+           "search",
+           "place_trend",
+           "check",         # コマンドリストの確認
+           "escape"         # プログラムの終了
+           )
+# Pytwitter
 # tweet()
 # home_timeline()
 # user_timeline()
-# research()
-place_trend()
+# followers_list()
+# search()
+# place_trend()
+
+twitter_main()
 
 # ログに書き出す
 # with open("PytwitterLog.log", "a+", encoding="utf-8") as f:
